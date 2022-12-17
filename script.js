5d5ccbfcb340add31fe8455e2b7c63a65fb7c17c
Princerwtt <109853314+Princerwtt@users.noreply.github.com>
2022-12-17 18:05:47 +0530

    1 file modified

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let arr = [1, 2, 7, 9, 10, 15];
 let smallest = arr[0];
-for (let i = 1; i < arr.length; i++) {
+for (let i = 0; i < arr.length; i++) {
   if (arr[i] < smallest) {
     smallest = arr[i];
   }
