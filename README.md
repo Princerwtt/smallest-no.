7e68f93e127feb36b7594cf3f7fc325c4604faef
Princerwtt <109853314+Princerwtt@users.noreply.github.com>
2022-12-17 14:27:09 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# smallest-no.
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-cxupeh)
---
+++ script.js
@@ -0,0 +1,8 @@
+let arr = [1, 2, 7, 9, 10, 15];
+let smallest = arr[0];
+for (let i = 1; i < arr.length; i++) {
+  if (arr[i] < smallest) {
+    smallest = arr[i];
+  }
+}
+console.log(smallest);
